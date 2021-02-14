9f919314c03746ebef54edccd08d771c93f82c29
oen142 <oen142@naver.com>
2021-02-14 23:45:02 +0900

    Initial commit

---
+++ src/main/java/com/wani/restclienttutorial/RestClientTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.restclienttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/restclienttutorial/RestClientTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.restclienttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
